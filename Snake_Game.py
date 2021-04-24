dc0b24a29fb9a72b32c48c1ccf54e91446f3f8a2
Shariq Aftab <77614011+Shariqaftab20@users.noreply.github.com>
2021-04-24 15:12:10 +0530

    Update Snake_Game.py

--- Snake_Game.py
+++ Snake_Game.py
@@ -1,9 +1,10 @@
 import pygame
+import random
 pygame.init()
 
 #color
 white=(255,255,255)
-red=(255,255,0)
+red=(255,0,0)
 black=(0,0,0)
 
 #screen dimention
@@ -16,9 +17,17 @@ pygame.display.update()
 
 exit_game=False
 quite_game=False
+food_x=random.randint(0,screen_width)
+food_y=random.randint(0,screen_height)
+velocity_x=0
+velocity_y=0
 snake_x=45
 snake_y=10
-snake_size=10
+food_size_x=10
+food_size_y=10
+snake_size_x=10
+snake_size_y=10
+score=0
 fps=30
 
 clock=pygame.time.Clock()
@@ -27,25 +36,35 @@ while not exit_game:
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
             exit_game=True
+        if event.type==pygame.KEYDOWN:
             #Right
-        if event.type==pygame.KEYDOWN:
             if event.key==pygame.K_RIGHT:
-                snake_x=snake_x+10
+                velocity_x=5
+                velocity_y=0
             #Down
-        if event.type==pygame.KEYDOWN:
             if event.key==pygame.K_DOWN:
-                snake_y=snake_y+10
+                velocity_x = 0
+                velocity_y = 5
             # Up
-        if event.type==pygame.KEYDOWN:
             if event.key==pygame.K_UP:
-                snake_y=snake_y-10
+                velocity_x = 0
+                velocity_y = -5
             # Left
-        if event.type==pygame.KEYDOWN:
             if event.key==pygame.K_LEFT:
-                snake_x=snake_x-10
+                velocity_x = -5
+                velocity_y = 0
+    if abs(snake_x-food_x)<6 and abs(snake_y-food_y)<6:
+        snake_size_x+=10
+        score+=1
+        print("Score:",score)
+        food_x = random.randint(0, screen_width)
+        food_y = random.randint(0, screen_height)
 
+    snake_x+=velocity_x
+    snake_y+=velocity_y
     gameWindow.fill(white)
-    pygame.draw.rect(gameWindow,black,[snake_x,snake_y,snake_size,snake_size])
+    pygame.draw.rect(gameWindow,red,[food_x,food_y,food_size_x,food_size_y])
+    pygame.draw.rect(gameWindow,black,[snake_x,snake_y,snake_size_x,snake_size_y])
     pygame.display.update()
     clock.tick(fps)
 
